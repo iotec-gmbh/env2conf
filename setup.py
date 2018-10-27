ab259e1dc8e7d880d50f9235f8ac02b6ef13af9a
Sven Haardiek <sven@haardiek.de>
2018-10-27 21:00:22 +0200

    Updated setup.py
    Signed-off-by: Sven Haardiek <sven@haardiek.de>

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
+    name="env2conf",
     version="0.1",
     author="Sven Haardiek",
     author_email="sven@haardiek.de",
     description="Python library to override configuration entries with environment variables",  # noqa 501
-    name="env2conf",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iotec-gmbh/env2conf",
     packages=["env2conf"],
     test_suite="tests",
 )
